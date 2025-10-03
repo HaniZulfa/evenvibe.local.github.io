e9567b18df546076c35d444f53ba0c8390810e27
Hani Zulfa <hani.zulfaa24@gmail.com>
2025-10-03 16:03:01 +0700

    Add files via upload

---
+++ EvenvibeLocalWeb/js/script.js
@@ -0,0 +1,18 @@
+// Toggle class active
+// Perbaikan: Gunakan selektor class (.)
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar side bar
+const hamburgermenu = document.querySelector(".navbar-nav");
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
